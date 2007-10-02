bb2109a1545b4e5a079bf9f6355409f49efbb26f
rickhelmus <rickhelmus@4b93ed76-1a02-0410-86fb-e1fd6f5b242a>
2007-10-02 22:12:05 +0000

    First doc updates, mainly mentioning gtk frontend, fixes of little grammar/spelling mistakes and restructering of the build installer chapter

--- trunk/src/docs/mkdoc.sh
+++ trunk/src/docs/mkdoc.sh
@@ -2,4 +2,4 @@
 #makeinfo --html --css-include=styles.css nixstaller.texi
 OUTDIR=../../doc
 texi2html nixstaller.texi --split=chapter --subdir=$OUTDIR --css-include=styles.css --no-sec-nav
-cp fltk.png ncurs.png $OUTDIR
+cp fltk.png ncurs.png gtk.png $OUTDIR
